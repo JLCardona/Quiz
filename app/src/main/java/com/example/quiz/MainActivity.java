195b350c92ebad1bd4cecacf28472fc41a4c73ce
JLCardona <jcardona@cifpfbmoll.eu>
2021-10-15 13:29:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_cheetah, true),
+            new Question(R.string.question_alligator, false),
+            new Question(R.string.question_hippo, false),
+            new Question(R.string.question_bats, true),
+            new Question(R.string.question_polar, true),
+
+
+    };
+    private int mCurrentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+        mTrueButton = (Button) findViewById(R.id.true_button);
+    }
+
+
+}
